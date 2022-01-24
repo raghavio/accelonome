27e3e9b8636df275c3bd2bf4d7d6ce02ebaf95b6
Raghav <raghav_ftw@hotmail.com>
2022-01-25 04:34:03 +0530

    Added vibration option

--- accelonome.js
+++ accelonome.js
@@ -19,6 +19,7 @@ const vueApp = {
             tempoChangeTrigger: 'bar', // tempo should get changed on Bar by default.
             lastTempoChangeTime: null, // time when user clicked play.
             tickSound: "metronome_1",
+            vibrateOn: false,
         }
     },
     methods: {
@@ -35,6 +36,8 @@ const vueApp = {
             });
             this.scheduledBeats = [];
             $('.dial').stop();
+            if (this.vibrateOn)
+                navigator.vibrate(0);
         },
         play() {
             if (!playedEmptyBuffer) {
@@ -48,7 +51,7 @@ const vueApp = {
             }
             if (!SOUNDS[this.tickSound].buffer || !SOUNDS[this.tickSound + '_accent'].buffer)
                 return;  // sound not loaded yet. can't play.
-            
+
             if (!this.tempo) {
                 this.tempo = this.startTempo;
                 this.userEnteredTempo = this.startTempo;
@@ -113,6 +116,15 @@ const vueApp = {
                     }
                 });
             }
+            // schedule vibration pattern
+            if (this.vibrateOn) {
+                let vibrationPatterns = [];
+                for (let beat = 1; beat <= this.beats; beat++) {
+                    vibrationPatterns.push(200, (secondsPerBeat - 0.2) * 1000);
+                }
+                navigator.vibrate(vibrationPatterns);
+            }
+
             timerWorker.postMessage({ eventName: "scheduleBar", inSeconds: next_bar_to_be_scheduled_in_seconds });
         },
         barCompleted() {
